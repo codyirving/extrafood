06729ab45816a6f9fa493c0382a11dfbd113123b
Cody <cody@codyi.net>
2018-08-13 18:39:26 -0700

    added claim functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import NavBar from "./components/navbar";
 import "./App.css";
 
@@ -6,46 +7,112 @@ import { Badge } from "react-bootstrap";
 
 class Listing extends Component {
   state = {
-    showDetails: false
+    showDetails: false,
+    showClaimDetails: false
   };
   moreInfo = () => {
     console.log(this.props.listing);
     this.setState({ showDetails: !this.state.showDetails });
   };
+  claimListing = () => {
+    console.log("claiming listing");
+    this.setState({ showClaimDetails: !this.state.showClaimDetails });
+  };
+  postClaim = e => {
+    e.preventDefault();
+    const payload = {
+      _id: this.props.listing._id,
+      claimed: "true",
+      claimedDate: new Date()
+    };
+
+    const postData = (url = "", data = {}) => {
+      // Default options are marked with *
+      return fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, same-origin, *omit
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+          // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      })
+        .then(response => response.json()) // parses response to JSON
+        .catch(error => console.error(`Fetch Error =\n`, error));
+    };
+
+    postData("http://localhost:3001/foodlistings/claim", payload)
+      .then(data => console.log(data)) // JSON from `response.json()` call
+      .catch(error => console.error(error));
+  };
+
   render() {
     const { dateExpires, itemDescription, datePosted } = this.props.listing;
     return (
       //TODO Automatically filter expiredListings
       <div className="row listing">
-        <div className="row column-headings">
-          <div className="col-3">Date Listed</div>
-          <div className="col-3">Food Description</div>
-          <div className="col-3">Date Expires</div>
-          <div className="col-3">More Info</div>
+        <div className="row basic-info">
+          <div className="col-3">
+            <div className="row column-label">Date Listed</div>
+            <div className="datePosted row">{datePosted}</div>
+          </div>
+          <div className="col-3">
+            <div className="row column-label">Food Description</div>
+            <div className="itemDescription row">{itemDescription}</div>
+          </div>
+          <div className="col-3">
+            <div className="row column-label">Date Expires</div>
+            <div className="dateExpires row">
+              {dateExpires === undefined ? "No expiration" : dateExpires}
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="row column-label">More Info</div>
+            <div className="moreInfo row">
+              <button onClick={this.moreInfo}>Interested!</button>
+            </div>
+          </div>
         </div>
-        <div className="dateExpires col-3">
-          {dateExpires === undefined ? "No expiration" : dateExpires}
-        </div>
-        <div className="itemDescription col-3">{itemDescription}</div>
-        <div className="datePosted col-3">{datePosted}</div>
-        <div className="moreInfo col-3">
-          <button onClick={this.moreInfo}>Interested!</button>
-        </div>
+
         {this.state.showDetails && (
-          <div className="row moreInfo">
-            <div className="pickupLocation col-3">
-              {this.props.listing.pickupLocation}
+          <div>
+            <div className="row moreInfo">
+              <div className="pickupLocation col-3">
+                {this.props.listing.pickupLocation}
+              </div>
+              <div className="listerContact-address col-3">
+                {this.props.listing.listerContact.address}
+              </div>
+              <div className="listerContact-phoneNumber col-3">
+                {this.props.listing.listerContact.phoneNumber}
+              </div>
+              <div className="listerContact-names col-3">
+                {this.props.listing.listerContact.nameFirst}{" "}
+                {this.props.listing.listerContact.nameLast}
+              </div>
             </div>
-            <div className="listerContact-address col-3">
-              {this.props.listing.listerContact.address}
+            <div className="claim-listing row">
+              <button onClick={this.claimListing}>Claim this listing!</button>
             </div>
-            <div className="listerContact-phoneNumber col-3">
-              {this.props.listing.listerContact.phoneNumber}
-            </div>
-            <div className="listerContact-names col-3">
-              {this.props.listing.listerContact.nameFirst}{" "}
-              {this.props.listing.listerContact.nameLast}
-            </div>
+            {this.state.showClaimDetails && (
+              <div>
+                <div className="claim-disclaimer row">
+                  {" "}
+                  By claiming this listing, you agree to the terms and
+                  conditions{" "}
+                </div>
+                <div className="agree-to-conditions">
+                  <form>
+                    <input type="checkbox" />
+                    <button onClick={this.postClaim}>Claim it!</button>
+                  </form>
+                </div>
+              </div>
+            )}
           </div>
         )}
       </div>
@@ -85,7 +152,15 @@ class SearchListings extends Component {
     );
   }
 }
-
+class RefreshData extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.props.onClick()}>REFRESH LISTINGS</button>
+      </div>
+    );
+  }
+}
 class App extends Component {
   state = {
     filterWords: ["tomatoes"],
@@ -109,7 +184,11 @@ class App extends Component {
     console.log("prevProps", prevProps);
     console.log("prevState", prevState);
   }
-
+  refreshData = () => {
+    fetch("http://localhost:3001/foodlistings/")
+      .then(response => response.json())
+      .then(data => this.setState({ listings: data, filteredListings: data }));
+  };
   handleSearch = input => {
     console.log("handling Search: " + input);
     const filteredList = this.state.listings.filter(listing => {
@@ -128,6 +207,7 @@ class App extends Component {
         <NavBar numberOfItems={this.state.filteredListings.length} />
         <main className="container">
           <SearchListings onClick={this.handleSearch} />
+          <RefreshData onClick={this.refreshData} />
           <Badge>42</Badge>
           <Listings
             listings={this.state.filteredListings}
